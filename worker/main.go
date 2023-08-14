15b77aefc878426a5df3f52e4f9f294ed72e2c54
dragonman164 <sanidhiyafirefox123@gmail.com>
2023-08-14 22:37:29 +0530

    request task by worker

--- worker/main.go
+++ worker/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"GoMapReduce/common/rpc"
+	"GoMapReduce/coordinator/coordinatorlib"
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+)
 
 func main() {
-	fmt.Printf("hello world")
+	uuid := uuid.New()
+	task := &coordinatorlib.Task{}
+	rpc.Call("Coordinator.RequestTask", uuid.String(), &task)
+	log.Println(fmt.Sprintf("%#v", task))
+
 }
